1c69c46441a86ebda1cd1b91f650e10c55311453
Christos <xrist.giann@gmail.com>
2023-02-17 14:49:31 +0200

    navabar fixed and made some css for navbar

--- src/api/user.js
+++ src/api/user.js
@@ -1,10 +1,10 @@
 import {createHeaders} from './customHeader'
 
-const API_URL = process.env.REACT_APP_API_URL
+const API_URL = process.env.REACT_APP_API_URL + '/users'
 
 const checkForUser = async (username) => {
     try{
-        const response = await fetch(`${API_URL}/users?username=${username}`);  //trying to connect with the api and checks if username exists
+        const response = await fetch(`${API_URL}?username=${username}`);  //trying to connect with the api and checks if username exists
         if(!response.ok){  //if connection fail
             throw new Error('Could not connect to api.'); //throw an error
         }
@@ -17,7 +17,7 @@ const checkForUser = async (username) => {
 
 const createUser = async (username) => {
     try{
-        const response = await fetch(`${API_URL}/users`, { //trying to connect with the api
+        const response = await fetch(API_URL, { //trying to connect with the api
             method:'POST',                      //with POST method,
             headers: createHeaders(),           //the custom headers
             body: JSON.stringify({              //and the json body of a new user in string
